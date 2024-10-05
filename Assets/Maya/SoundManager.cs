f9158e4adb4936e07b4ebd44f1242cd994b5ced8
mayarachels <mayarachels1@gmail.com>
2024-10-05 19:35:01 -0400

    Sound Manager

---
+++ Assets/Maya/SoundManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundType
+{
+    EXPLOSION,
+    FLARE,
+    FOOTSTEPS,
+    GUNSHOT,
+    HEARTBEAT,
+    BREATHING,
+    METAL,
+    RADIO,
+    UNLOCK,
+    WAR
+
+}
+[RequireComponent(typeof(AudioSource))]
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public static void PlaySound(SoundType sound, float volume = 1)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
+}
