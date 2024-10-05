4982279ff0923db18398a9fe179177e3c684c079
BloonZhang <BloonZhang@gmail.com>
2024-10-05 15:03:12 -0400

    enemy affects post processing volume

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using StarterAssets;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class EnemyController : MonoBehaviour
 {
     public static EnemyController Instance;
+
+    [SerializeField] private Volume basePPVol;
+    [SerializeField] private Volume monsterPPVol;
+
+    private bool isActive;
+    private float spawnDistFromPlayer;
+    private float distanceAdjustPP = 10f;
 
     void Awake()
     {
@@ -15,6 +24,15 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         Disappear();
+    }
+
+    void Update()
+    {
+        if (isActive)
+        {
+            float distanceFromPlayer = Vector3.Distance(transform.position, FirstPersonController.Instance.transform.position);
+            SetPostProcessBlend(1 - ((distanceFromPlayer - distanceAdjustPP) / (spawnDistFromPlayer - distanceAdjustPP)));
+        }
     }
 
     // public functions
@@ -33,9 +51,21 @@ public class EnemyController : MonoBehaviour
     private void Appear()
     {
         gameObject.SetActive(true);
+        isActive = true;
+        spawnDistFromPlayer = Vector3.Distance(transform.position, FirstPersonController.Instance.transform.position);
     }
     private void Disappear()
     {
         gameObject.SetActive(false);
+        isActive = false;
+        SetPostProcessBlend(0);
+    }
+    private void SetPostProcessBlend(float percentage)
+    {
+        percentage = Mathf.Clamp(percentage, 0f, 1f);
+        Debug.Log("setting post process to " + percentage);
+
+        //basePPVol.weight = (1f - percentage);
+        monsterPPVol.weight = (percentage);
     }
 }
