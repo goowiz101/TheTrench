72e796e9c7b9617fb079b1679924e1f8238dccf6
BloonZhang <BloonZhang@gmail.com>
2024-10-06 12:01:47 -0400

    Fix wood scale on fence. Add grenade interactible

--- Assets/Scripts/Interactables/Grenade.cs
+++ Assets/Scripts/Interactables/Grenade.cs
@@ -7,12 +7,14 @@ public class Grenade : MonoBehaviour
 {
     private bool isPickedUp = false;
 
+    private Vector3 goalPosition;
+
     private void Update()
     {
         if (isPickedUp)
         {
             // TODO: timer, not hardcoded time duh
-            transform.localPosition = Vector3.MoveTowards(transform.localPosition, Vector3.zero, 60f * Time.deltaTime);
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, goalPosition, 60f * Time.deltaTime);
             transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0f,0f,0f), 20f * Time.deltaTime);
 
             // DEBUG
@@ -25,6 +27,7 @@ public class Grenade : MonoBehaviour
 
     public void PickUpGrenade()
     {
+        goalPosition = new Vector3(0, 0, 0.2f);
         transform.SetParent(FirstPersonController.Instance.handTransform);
         FirstPersonController.Instance.GiveGrenade();
         isPickedUp = true;
